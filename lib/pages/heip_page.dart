e3b434541de6ab2899eee3228f79bc34ad0fd1bc
Khemjira270449 <khemchira.r@ku.th>
2026-09-14 13:47:35 +0700

    Add files via upload

---
+++ lib/pages/heip_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HelpPage extends StatelessWidget {
+  const HelpPage({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contact'),
+        backgroundColor: Colors.grey.shade300,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: const Color.fromARGB(255, 164, 213, 173),
+        child: Center(
+          child: Text(
+            "Help Page",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
